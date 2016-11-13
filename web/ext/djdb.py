985d84af377f2c222d9494d9796a8f98c2eca0e0
Brian Millham <bmillham@gmail.com>
2016-11-12 23:35:24 -0500

    Cleaned up tabs/spaces in root.

--- web/ext/djdb.py
+++ web/ext/djdb.py
@@ -5,31 +5,31 @@ from web.app.djrq.model.lastplay import DJs
 
 
 class FakeContext:
-	""" A fake session, just used to query the database """
-	def __init__(self):
-		self.db = {}
+    """ A fake session, just used to query the database """
+    def __init__(self):
+        self.db = {}
 
 class DJDatabaseExtension(DatabaseExtension):
-	_needs = {'djhost'}
-	_provides = {'djdb', 'db'}
+    _needs = {'djhost'}
+    _provides = {'djdb', 'db'}
 
-	def __init__(self, default=None, session=None):
-		self.needs = set(self._needs)
-		self.provides = set(self._provides)
+    def __init__(self, default=None, sessions=None):
+        self.needs = set(self._needs)
+        self.provides = set(self._provides)
 
-		engines = {'lastplay': SQLAlchemyConnection(lastplay_url)}
-		context = FakeContext()
-		engines['lastplay'].start(context)
-		djs = engines['lastplay'].Session.query(DJs).filter(DJs.hide_from_menu == 0)
+        engines = {'lastplay': SQLAlchemyConnection(lastplay_url)}
+        context = FakeContext()
+        engines['lastplay'].start(context)
+        djs = engines['lastplay'].Session.query(DJs).filter(DJs.hide_from_menu == 0)
 
-		for d in djs:
-			# TODO: make this configurable
-			server = "themaster.millham.net" if d.server == "localhost" else d.server
-			url = "mysql://{}:{}@{}/{}?charset=utf8".format(d.user, d.password, server, d.db)
-			engines[d.dj.lower()] = SQLAlchemyConnection(url)
-			print(d.user, d.password, server)
+        for d in djs:
+            # TODO: make this configurable
+            server = "themaster.millham.net" if d.server == "localhost" else d.server
+            url = "mysql://{}:{}@{}/{}?charset=utf8".format(d.user, d.password, server, d.db)
+            engines[d.dj.lower()] = SQLAlchemyConnection(url)
+            print(d.user, d.db, server)
 
-		engines['lastplay'].stop(context)
-		#if default:
-		#	engines['default'] = default
-		super(DJDatabaseExtension, self).__init__(default, **engines)
+        engines['lastplay'].stop(context)
+        if sessions:
+            engines['sessions'] = sessions
+        super(DJDatabaseExtension, self).__init__(default, **engines)
