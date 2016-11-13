5062fbc6621c83a23f58f287c075f6fe86396bf2
Brian Millham <bmillham@gmail.com>
2016-11-12 23:35:54 -0500

    Cleaned up tabs/spaces in lastplay

--- web/app/djrq/lastplayed.py
+++ web/app/djrq/lastplayed.py
@@ -3,17 +3,17 @@
 from .templates.lastplayed import lastplayedtemplate
 
 class LastPlayed:
-	__dispatch__ = 'resource'
-	__resource__ = 'lastplayed'
+    __dispatch__ = 'resource'
+    __resource__ = 'lastplayed'
 
-	def __init__(self, context, *arg, **args):
-		self._ctx = context
-		self.queries = self._ctx.queries
+    def __init__(self, context, *arg, **args):
+        self._ctx = context
+        self.queries = self._ctx.queries
 
-	def get(self, *arg, **args):
-		lplist = self.queries.get_last_played()
-		if not self._ctx.session.username:
-			self._ctx.session.username='someusername'
-		else:
-			print("Got a username from the session", self._ctx.session.username)
-		return lastplayedtemplate("50 Last Played", self._ctx, lplist)
+    def get(self, *arg, **args):
+        lplist = self.queries.get_last_played()
+        if not self._ctx.session.username:
+            self._ctx.session.username='someusername'
+        else:
+            print("Got a username from the session", self._ctx.session.username)
+        return lastplayedtemplate("50 Last Played", self._ctx, lplist)
