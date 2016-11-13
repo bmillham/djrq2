d8154f81f9df752ade0e669751b2863747e87b72
Brian Millham <bmillham@gmail.com>
2016-11-13 00:05:54 -0500

    Moved the Session document declaration into a proper model.

--- web/app/djrq/__main__.py
+++ web/app/djrq/__main__.py
@@ -13,21 +13,14 @@ from web.ext.djhost import DJHostExtension
 from web.ext.db import DatabaseExtension
 from web.db.mongo import MongoDBConnection
 from web.ext.session import SessionExtension
-from web.session.mongo import MongoSession, MongoSessionStorage
-from marrow.mongo.core import Field
-from marrow.mongo import String
+from web.session.mongo import MongoSession
+
+from web.app.djrq.model.session import Session
 
 from .root import Root
 
 SESSION_URI = 'mongodb://localhost/djrq2'
 SESSION_SECRET = 'xyzzy'
-
-class Session(MongoSessionStorage):
-    __collection__ = 'sessions'
-
-    username = String(default=None)
-    sitenick = String(default=None)
-    #usertheme = String(default=None)
 
 app=Application(Root, extensions=[
         AnnotationExtension(),
---
+++ web/app/djrq/model/session/__init__.py
@@ -0,0 +1,9 @@
+from web.session.mongo import MongoSessionStorage
+from marrow.mongo import String
+
+class Session(MongoSessionStorage):
+    __collection__ = 'sessions'
+
+    username = String(default=None)
+    sitenick = String(default=None)
+    #usertheme = String(default=None)
