a4e059035369107d5b33562b1e06ce7060c08f25
Brian Millham <bmillham@gmail.com>
2016-11-12 23:38:55 -0500

    Cleaned up tabs/spaces in root.

--- web/app/djrq/root.py
+++ web/app/djrq/root.py
@@ -10,43 +10,43 @@ from .model.common import Listeners
 import os
 
 class Root:
-	__dispatch__ = 'resource'
+    __dispatch__ = 'resource'
 
-	# Import the album, artist, stats, requests and lastplayed endpoints.
-	from .album import Album as album, Album
-	from .artist import Artist as artist, Artist
-	from .stats import Stats as stats, Stats
-	from .requests import Requests as requests, Requests
-	from .lastplayed import LastPlayed as lastplayed
-	from .whatsnew import WhatsNew as whatsnew
+    # Import the album, artist, stats, requests and lastplayed endpoints.
+    from .album import Album as album, Album
+    from .artist import Artist as artist, Artist
+    from .stats import Stats as stats, Stats
+    from .requests import Requests as requests, Requests
+    from .lastplayed import LastPlayed as lastplayed
+    from .whatsnew import WhatsNew as whatsnew
 
 
-	public = static(os.path.join(os.path.dirname(__file__), 'public'))
+    public = static(os.path.join(os.path.dirname(__file__), 'public'))
 
-	def __init__(self, context=None, collection=None, record=None):
-		""" Setup basic stuff needed for all pages """
-		self._ctx = context
+    def __init__(self, context=None, collection=None, record=None):
+        """ Setup basic stuff needed for all pages """
+        self._ctx = context
 
-	def get(self, *arg, **args):
-		""" Handle other endpoints not imported """
-		if len(arg) == 0:
-			return HTTPFound(location="/lastplayed") # Make lastplayed the default
-		else:
-			return notfound(self._ctx, arg[0])
+    def get(self, *arg, **args):
+        """ Handle other endpoints not imported """
+        if len(arg) == 0:
+            return HTTPFound(location="/lastplayed") # Make lastplayed the default
+        else:
+            return notfound(self._ctx, arg[0])
 
-	def post(self, content, *arg, **args):
-		print("Got a post", content, arg, args )
-		if content == 'search':
-			if 'stext' not in args:
-				l = self._ctx.queries.advanced_search(search_for=args['advsearchtype'].lower(), phrase=args['advsearchtext'])
-				tl = tracklist(self._ctx, l, dataonly=True, phrase='{advsearchtype}: {advsearchtext}'.format(**args))
-				r = ""
-				for i in tl:
-					r += i
-				return {'html' : r}
-				#return tracklist(self._ctx, l)
-				#return {'html': 'Search for {advsearchtype} {advsearchtext}'.format(**args)}
-			else:
-				l = self._ctx.queries.full_text_search(phrase=args['stext'])
-				return tracklist(self._ctx, l, phrase=args['stext'])
-		return notfound(self._ctx, content)
+    def post(self, content, *arg, **args):
+        print("Got a post", content, arg, args )
+        if content == 'search':
+            if 'stext' not in args:
+                l = self._ctx.queries.advanced_search(search_for=args['advsearchtype'].lower(), phrase=args['advsearchtext'])
+                tl = tracklist(self._ctx, l, dataonly=True, phrase='{advsearchtype}: {advsearchtext}'.format(**args))
+                r = ""
+                for i in tl:
+                    r += i
+                return {'html' : r}
+                #return tracklist(self._ctx, l)
+                #return {'html': 'Search for {advsearchtype} {advsearchtext}'.format(**args)}
+            else:
+                l = self._ctx.queries.full_text_search(phrase=args['stext'])
+                return tracklist(self._ctx, l, phrase=args['stext'])
+        return notfound(self._ctx, content)
