6f0d53e123bac2a615c9f0e3808fed373e957274
Brian Millham <bmillham@gmail.com>
2016-11-19 22:09:13 -0500

    Fixed request/mistags button to always display on a single line.

--- web/app/djrq/templates/helpers/helpers.py
+++ web/app/djrq/templates/helpers/helpers.py
@@ -5,28 +5,31 @@
 : def request_link row, td=False
     : using _td td
         ${row.title}
-        <button class="btn btn-xs btn-info pull-right m_${row.id}"
-                id='m_${row.id}'
-                data-toggle='modal'
-                data-target='#mistagModal'
-                data-title='${row.title}'
-                data-artist='${row.artist.fullname}'
-                data-album='${row.album.fullname}'
-                data-tid='${row.id}'
-                ${"disabled='disabled'" if len(row.mistags) > 0 else ""}
-                >Mistag</button>
-        <button class="btn btn-xs btn-primary pull-right r_${row.id}"
-                id='r_${row.id}'
-                data-toggle="modal"
-                data-target="#requestModal"
-                data-title='${row.title}'
-                data-tid='${row.id}'
-                # Disable the button if there are requests
-                ${"disabled='disabled'" if len(row.new_requests) > 0 else ""}
-                >
-                #${"Request" if len(row.new_requests) == 0 else "Requested"}
-                Request
-        </button>
+        <div class="pull-right">
+         <button class="btn btn-xs btn-primary r_${row.id}"
+                 id='r_${row.id}'
+                 data-toggle="modal"
+                 data-target="#requestModal"
+                 data-title='${row.title}'
+                 data-tid='${row.id}'
+                 # Disable the button if there are requests
+                 ${"disabled='disabled'" if len(row.new_requests) > 0 else ""}
+                 >
+                 #${"Request" if len(row.new_requests) == 0 else "Requested"}
+                 Request
+         </button>
+         <button class="btn btn-xs btn-info m_${row.id}"
+                 id='m_${row.id}'
+                 data-toggle='modal'
+                 data-target='#mistagModal'
+                 data-title='${row.title}'
+                 data-artist='${row.artist.fullname}'
+                 data-album='${row.album.fullname}'
+                 data-tid='${row.id}'
+                 ${"disabled='disabled'" if len(row.mistags) > 0 else ""}
+                 >Mistag
+         </button>
+        </div>
     : end
 : end
 
