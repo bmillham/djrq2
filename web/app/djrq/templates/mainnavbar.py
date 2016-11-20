c6995569f7925263f34b6d0fb873043d98b8e368
Brian Millham <bmillham@gmail.com>
2016-11-19 23:05:04 -0500

    Fixed navbar so it shows a little better on small devices, but still isn't usable on small.

--- web/app/djrq/templates/mainnavbar.py
+++ web/app/djrq/templates/mainnavbar.py
@@ -70,7 +70,7 @@
         <li #{" class='active'" if resource == 'stats' else ''}><a href="/stats">Stats</a></li>
         <li><a href="#" data-toggle='modal' data-target='#suggestionModal'>Suggestion</a></li>
        </ul>
-       <form class='navbar-form navbar-left' action='/search' method='post' role='search'>
+       <form class='navbar-form navbar-left hidden-xs hidden-sm' action='/search' method='post' role='search'>
         <div class='form-group'>
          <div class='input-group'>
           <span class='input-group-addon'>
@@ -84,7 +84,7 @@
         </div>
        </form>
 
-       <ul class='nav navbar-nav navbar-right'>
+       <ul class='nav navbar-nav navbar-right hidden-xs hidden-sm'>
         <li name='usericon' style='${"display:none" if not ctx.session.sitenick else ""}'><a href="#"><span class='glyphicon glyphicon-user'></span></a></li>
         <li class='dropdown'>
          <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>
@@ -102,6 +102,9 @@
             </li>
 
         : end
+       </ul>
+       <ul class='nav navbar-nav navbar-right hidden-md hidden-lg'>
+        <li><a href="#"><span class='glyphicon glyphicon-option-vertical'></span></a></li>
        </ul>
       </div>
      </div>
