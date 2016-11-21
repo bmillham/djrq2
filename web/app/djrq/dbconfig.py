73e2bf1f962c1076209e9e750f3b3482329a692d
Brian Millham <bmillham@gmail.com>
2016-11-21 15:43:55 -0500

    Removed unused dbconfig.py

--- web/app/djrq/dbconfig.py
+++
@@ -1,5 +0,0 @@
-#TODO: make this into a class that reads the info from a file outside the
-#web space. Maybe...
-
-# Edit this to the real database url
-lastplay_url = 'mysql://user:passwords@dbserver/lastplay'
