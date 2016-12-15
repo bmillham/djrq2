5c76b158a7f325b547bec7ee28ee43275246de77
Brian Millham <bmillham@gmail.com>
2016-12-14 23:06:38 -0500

    Fixed url for subscribe.js to remove the hardcoded url.

--- web/app/djrq/public/js/subscribe.js
+++ web/app/djrq/public/js/subscribe.js
@@ -1,4 +1,6 @@
-var sub = new NchanSubscriber(url='http://websock.gelth.local/sub?id=' + window.location.hostname.split('.')[0], opt={subscriber:'websocket'});
+var dj = window.location.hostname.split('.')[0];
+var url = 'http://' + window.location.hostname.split(':')[0] + '/sub?id=' + dj;
+var sub = new NchanSubscriber(url=url, opt={subscriber:'websocket'});
 
 sub.on("message", function(message, message_metadata) {
   var m = JSON.parse(message)
