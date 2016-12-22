5c58fe1f0413a3d752f979c66685e9334ed1dc54
Brian Millham <bmillham@gmail.com>
2016-12-22 01:40:32 -0500

    Reworked the footer for all pages.

--- web/app/djrq/templates/footer.py
+++ web/app/djrq/templates/footer.py
@@ -6,12 +6,18 @@
 
 : def site_footer styles=[], scripts=[]
     <center>
-     <h5>&copy;2010-${datetime.now().year} Brian Millham</h5>
-     <h5>Glyphicons courtesy of <a href='http://glyphicons.com'>glyphicons.com</a></h5>
-     #{colophon}
-     <h6>HTML/css/js Famework: <a href='http://getbootstrap.com'>Bootstrap</a></h6>
-     <h6>Bootstrap Themes: <a href='http://bootswatch.com'>Bootswatch</a></h6>
-     <h6><i>This site is for informational purposes only. No music files are hosted or shared on this site</i></h6>
+    <div class='container'>
+     <div class='row'>
+      <div class='col-md-4 center-block'><b>&copy;2010-${datetime.now().year} Brian Millham</b></div>
+      <div class='col-md-4 center-block'>#{colophon}</div>
+      <div class='col-md-4 center-block'><i>This site is for informational purposes only. No music files are hosted or shared on this site</i></div>
+     </div>
+     <div class='row'>
+      <div class='col-md-4'><h5>Glyphicons courtesy of <a href='http://glyphicons.com'>glyphicons.com</a></h5></div>
+      <div class='col-md-4'><h6>HTML/css/js Famework: <a href='http://getbootstrap.com'>Bootstrap</a></h6></div>
+      <div class='col-md-4'><h6>Bootstrap Themes: <a href='http://bootswatch.com'>Bootswatch</a></h6></div>
+     </div>
+    </div>
     </center>
     : use default_footer styles=styles, scripts=scripts
 : end
