7f7d955ce70d8d7c62540c772c5e0083281a39ab
Brian Millham <bmillham@gmail.com>
2017-01-04 21:32:45 -0500

    Added new extension to read user browsers language, and set the locale from that.

---
+++ web/ext/locale.py
@@ -0,0 +1,100 @@
+from babel import dates, numbers, units
+from datetime import datetime, timedelta
+from time import time
+
+#l = 'he_il' # Hebrew
+#l = 'ja_jp' # Japanese
+#l = 'ko_kr' # Korean
+#l = 'fr' # French
+#l = 'ar' # Arabic
+#l = 'zh_tw' # Chinese (traditional)
+#l = 'zh_cn' # Chinese (simplified)
+#l = 'ru_ru' # Russian
+
+class LocaleExtension:
+    """ Set the locale, and define locale functions """
+
+    needs = {'themes'}
+    provides = {'locale'}
+
+    def __init__(self, userlang='en_us'):
+        self.userlang = userlang
+
+    def prepare(self, context):
+        try:
+            langs = context.request.accept_language.header_value.split(',')
+        except:
+            langs = []
+        print('Langs', langs)
+        try:
+            lang = langs[0]
+        except:
+            lang = self.userlang
+        #if '-' not in lang:
+        #    lang += '_' + lang.upper()
+
+        self.userlocale = lang.replace('-', '_')
+        context.userlocale = self.userlocale
+
+        context.time_ago = self.time_ago
+        context.format_decimal = self.format_decimal
+        context.format_time = self.format_time
+        context.time_length = self.time_length
+        context.format_percent = self.format_percent
+        context.format_size = self.format_size
+        print('Locale prepared:', self.userlocale)
+
+    def time_ago(self, dtime, add_direction=True):
+        if type(dtime) != datetime:
+            dtime = datetime.fromtimestamp(time() - (60*60*24*dtime))
+        return dates.format_timedelta(dtime - datetime.utcnow(), threshold=2, add_direction=add_direction, locale=self.userlocale)
+
+    def format_decimal(self, number):
+        return numbers.format_decimal(number, locale=self.userlocale)
+
+    def format_time(self, seconds, is_avg=False):
+        try:
+            ft = dates.format_time(int(seconds), format='HH:mm:ss')
+        except:
+            return seconds
+        v = ft.split(':')
+        if v[0] == '00':
+            v.pop(0)
+            rv = ':'.join(v)
+        else:
+            rv = ft
+        if is_avg:
+            rv = '~' + rv
+        return rv
+
+    def time_length(self, tlen):
+        return dates.format_timedelta(timedelta(seconds=int(tlen)), locale=self.userlocale)
+
+    def format_percent(self, number):
+        return numbers.format_percent(number, format="#.#%", locale=self.userlocale)
+
+    def format_size(self, number):
+        if number is None:
+            return ""
+        s = 1024
+        kb = s
+        mb = kb * s
+        gb = mb * s
+        tb = gb * s
+
+        if number >= tb:
+            number /= tb
+            unit = 'terabyte'
+        elif number >= gb:
+            number /= gb
+            unit = 'gigabyte'
+        elif number >= mb:
+            number /= mb
+            unit = 'megabyte'
+        elif number >= kb:
+            number /= kb
+            unit = 'kilobyte'
+        else:
+            unit = 'byte'
+        return units.format_unit(number, unit, length='short', format="#.#", locale=self.userlocale)
+
