e7ac4c8509f0d3c2b91c7f27fe4338d6b9c741b3
Brian Millham <bmillham@gmail.com>
2017-01-14 18:30:03 -0500

    Improved the admin navbar with icons, and grouped site options together.

--- web/app/djrq/templates/admin/adminnavbar.py
+++ web/app/djrq/templates/admin/adminnavbar.py
@@ -25,7 +25,7 @@
        <ul class='nav navbar-nav'>
         <li #{" class='active'" if resource == None else ''}>
          <a class='navbar-brand' href='/admin'>
-          <span class='glyphicon glyphicon-cog'></span>&nbsp;${ctx.djname}&nbsp;
+          <span class='glyphicon glyphicon-cog' aria-hidden='true'></span>&nbsp;${ctx.djname}&nbsp;
           <span id='requestbutton' class='badge'>${ctx.format_decimal(ctx.requests_info.request_count)}</span>
          </a>
         </li>
@@ -37,13 +37,22 @@
           <li #{" class='active'" if resource == 'suggestions' else ''}>
            <a href="/admin/suggestions">Suggestions&nbsp;<span id='suggestionsbutton' class='badge'>${ctx.format_decimal(ctx.suggestions_count)}</span></a>
           </li>
-          <li #{" class='active'" if resource == 'showinfo' else ''}><a href='/admin/showinfo'>Show Info</a></li>
-          <li #{" class='active'" if resource == 'requestoptions' else ''}><a href='/admin/requestoptions'>Request Options</a></li>
-          : if ctx.databasetype == 'ampache'
-            <li #{" class='active'" if resource == 'catalogoptions' else ''}><a href='/admin/catalogoptions'>Catalog Selection</a></li>
-          : end
+          <li class='dropdown #{" active" if resource in ("showinfo", "requestoptions", "catalogoptions", "showhistory", "changepw") else ""}'>
+           <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>
+            <span class='glyphicon glyphicon-wrench' aria-hidden='true' aria-label='Site Options'></span><span class='caret'></span></a>
+           <ul class='dropdown-menu'>
+            <li #{" class='active'" if resource == 'showinfo' else ''}><a href='/admin/showinfo'>Show Info</a></li>
+            <li #{" class='active'" if resource == 'requestoptions' else ''}><a href='/admin/requestoptions'>Request Options</a></li>
+            : if ctx.databasetype == 'ampache'
+             <li #{" class='active'" if resource == 'catalogoptions' else ''}><a href='/admin/catalogoptions'>Catalog Selection</a></li>
+            : end
+            <li #{" class='active'" if resource == 'showhistory' else ""}><a href='/admin/showhistory'>View History</a></li>
+            <li #{" class='active'" if resource == 'changepw' else ""}><a href='/admin/changepw'>Change Password</a></li>
+           </ul>
+          </li>
+
           <li class='dropdown'>
-           <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Upload Files<span class='caret'></span></a>
+           <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'><span class='glyphicon glyphicon-cloud-upload' aria-hidden='true' aria-label='Upload Files'></span><span class='caret'></span></a>
            <ul class='dropdown-menu'>
             <li><a href='/admin/uploadfiles/private'>Your Private Area</a></li>
             <li><a href='/admin/uploadfiles/shared'>DJ Shared Area</a></li>
@@ -53,17 +62,15 @@
            <li #{" class='active'" if resource == 'updatedatabase' else ""}><a class='dbupdate' href='/admin/updatedatabase'>Update Database</a></li>
           : end
          : end
-          <li #{" class='active'" if resource == 'changepw' else ""}><a href='/admin/changepw'>Change Password</a></li>
-          <li #{" class='active'" if resource == 'showhistory' else ""}><a href='/admin/showhistory'>View History</a></li>
         : except AttributeError
          : pass
         : end
        </ul>
        <ul class='nav navbar-nav navbar-right'>
-        <li><a href="/lastplayed"><span class='glyphicon glyphicon-home'></span></a></li>
-        <li><a href='/admin/auth/?logout'><span class='glyphicon glyphicon-off'></span></a></li>
+        <li><a href="/lastplayed"><span class='glyphicon glyphicon-home' aria-hidden='true' aria-label='Home'></span></a></li>
+        <li><a href='/admin/auth/?logout'><span class='glyphicon glyphicon-log-out' aria-hidden='true' aria-label='Logoff'></span></a></li>
         : if ctx.listeners is not None
-         <li><a href='#'><span class='glyphicon glyphicon-headphones'></span>&nbsp;
+         <li><a href='#'><span class='glyphicon glyphicon-headphones' aria-hidden='true' aria-label='Listeners'></span>&nbsp;
                         <span id='listeners'>${ctx.listeners.current}</span>/<span id='maxlisteners'>${ctx.listeners.max}</span></a>
          </li>
         : end
