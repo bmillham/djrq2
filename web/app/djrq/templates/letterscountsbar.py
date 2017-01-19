dfdb03782e88ff5e8c8534a765126d4931fe2ecf
Brian Millham <bmillham@gmail.com>
2017-01-18 21:28:31 -0500

    Reworked the letter navigation using bootstrap list-group.

--- web/app/djrq/templates/letterscountsbar.py
+++ web/app/djrq/templates/letterscountsbar.py
@@ -10,26 +10,18 @@
     :   selected_letter = None
     : end
 
-    <div class="col-sm-1">
-     <div class="sidebar-nav">
-      <div class="sidebar-nav navbar navbar-default" role="navigation">
-       <div class="navbar-collapse collapse sidebar-navbar-collapse">
-        <ul class="navbar navbar-nav sidebar-nav">
-         : for row in letterscountslist
-            : if row.letter == ''
-                : print("Skip Letter: |{}|".format(row.letter), dir(row.letter))
-                : continue
-            :end
-         : l = quote_plus(row.letter)
-         <li style="text-align: center; width: 90px;" #{"class='active'" if selected_letter == row.letter else ""}>
-            <a href="/${ctx.resource.__resource__}/?letter=${l}" tip='${row.count}'>${row.letter}
-                <span class='badge'>${row.count}</span>
-            </a>
-         </li>
-         : end
-        </ul>
-       </div>
-      </div>
-     </div>
+    <div class="col-sm-1 list-group">
+     : for row in letterscountslist
+        : if row.letter == ''
+            : print("Skip Letter: |{}|".format(row.letter), dir(row.letter))
+            : continue
+        :end
+        : l = quote_plus(row.letter)
+        <a href="/${ctx.resource.__resource__}/?letter=${l}"
+           tip='${row.count}'
+           class='list-group-item #{"active" if selected_letter == row.letter else ""}'>
+           ${row.letter} <span class='badge'>${row.count}</span>
+        </a>
+     : end
     </div>
 : end
