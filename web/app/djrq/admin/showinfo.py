0b95dae178027a208b33c1c1d8fc602866e96ed2
Brian Millham <bmillham@gmail.com>
2017-01-20 21:20:46 -0500

    Changed show_time to save in UTC, instead of the users local timezone.

--- web/app/djrq/admin/showinfo.py
+++ web/app/djrq/admin/showinfo.py
@@ -1,6 +1,8 @@
 # encoding: utf-8
 
 from ..templates.admin.showinfo import showinfotemplate
+import datetime
+import pytz
 
 class ShowInfo:
     __dispatch__ = 'resource'
@@ -15,6 +17,8 @@ class ShowInfo:
         return showinfotemplate("Show Information", self._ctx,  showinfo=so)
 
     def post(self, *arg, **args):
+        dt = datetime.datetime.strptime(args['show_time'], '%Y-%m-%d %H:%M %z')
+        args['show_time'] = dt.astimezone(pytz.timezone('UTC')).strftime('%Y-%m-%d %H:%M %z')
         result = self.queries.save_siteoptions(**args)
         so = self.queries.get_siteoptions()
         return showinfotemplate("Show Information Saved", self._ctx,  showinfo=so, saved=True)
--- web/app/djrq/public/js/datetimepicker-enable.js
+++ web/app/djrq/public/js/datetimepicker-enable.js
@@ -1,3 +1,17 @@
+function pad(number, length){
+    var str = "" + number;
+    while (str.length < length) {
+        str = '0'+str;
+    }
+    return str;
+}
+
+var offset = new Date().getTimezoneOffset();
+offset = ((offset<0? "+":"-")+
+          pad(parseInt(Math.abs(offset/60)), 2)+
+          pad(Math.abs(offset%60), 2));
+
+
 $('.form_datetime').datetimepicker({
         //language:  'fr',
         weekStart: 1,
@@ -6,5 +20,6 @@ $('.form_datetime').datetimepicker({
         todayHighlight: 1,
         startView: 2,
         forceParse: 0,
-        showMeridian: 1
+        showMeridian: 1,
+        timezone: offset,
     });
