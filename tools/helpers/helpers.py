d5b416febe57bdfddce417d1f52c65b1e3ad2f8f
Brian Millham <bmillham@gmail.com>
2023-04-04 03:41:16 +0000

    Moved find_info to helpers module

--- tools/helpers/helpers.py
+++ tools/helpers/helpers.py
@@ -13,3 +13,75 @@ class Helpers(object):
             else:
                 s.pop(0) # Remove hour if 0
             return ':'.join(s)
+
+    def find_info(self, ctx, title):
+        """ Try and find a song when there are more than 3 fields in title"""
+
+        site_options = ctx.queries.get_siteoptions()
+        md_fields = site_options.metadata_fields.split(' - ')
+
+        if site_options.strict_metadata:
+            found_fields = title.split(' - ')
+            if len(found_fields) != len(md_fields):
+                print(f'Strict Metadata set and fields to not match for {title}')
+                return [None] * 3
+            else:
+                return found_fields
+
+        song_title = title.split(' - ')
+        num_fields = len(fields)
+        found = {}
+        for field in md_fields:
+            print('Working on', field, song_title)
+            val = []
+            while len(song_title) > 0:
+                val.append(song_title.pop(0))
+                l = ' - '.join(val)
+                if field == 'artist':
+                    a = ctx.queries.get_artist(l)
+                elif field == 'title':
+                    a = ctx.queries.get_title(found['artist'].id, l)
+                elif field == 'album':
+                    if type(found['artist']) == list:
+                        for art in found['artist']:
+                            a = ctx.queries.get_album(art.id, l)
+                            if a.count() > 0:
+                                break
+                    else:
+                        a = ctx.queries.get_album(found['artist'].id, l)
+                else:
+                    print(f'ERROR: Unknow field: {field}')
+                if a.count() > 0:
+                    if a.count() > 1:
+                        print('found more than one match for', field)
+                        found[field] = a.all()
+                    else:
+                        found[field] = a.one()
+                    break
+
+        titles = set()
+        if type(found['title']) == list:
+            for title in found['title']:
+                titles.add(title.title)
+        else:
+            titles.add(found['title'].title)
+        albums = set()
+        if type(found['album']) == list:
+            for album in found['album']:
+                albums.add(album.album.prename)
+        else:
+            albums.add(found['album'].album.prename)
+        if len(titles) == 1:
+            titles = titles.pop()
+        if len(albums) == 1:
+            albums = albums.pop()
+
+        song = ctx.queries.get_song_by_ata(found['artist'].fullname,
+                                           titles, albums)
+        if song.count() == 0:
+            print('Giving up!')
+            return None, None, None
+        else:
+            s = song.one()
+            print('Found song id', s.id)
+        return found['artist'].fullname, titles, albums
--- tools/monitor_icecast.py
+++ tools/monitor_icecast.py
@@ -65,78 +65,6 @@ parser.add_argument('-p', '--listen-mount-points', default='listen',
 args = parser.parse_args()
 helpers = Helpers()
 
-def find_info(ctx, title):
-    """ Try and find a song when there are more than 3 fields in title"""
-
-    site_options = ctx.queries.get_siteoptions()
-    md_fields = site_options.metadata_fields.split(' - ')
-
-    if site_options.strict_metadata:
-        found_fields = title.split(' - ')
-        if len(found_fields) != len(md_fields):
-            print(f'Strict Metadata set and fields to not match for {title}')
-            return [None] * 3
-        else:
-            return found_fields
-        
-    song_title = title.split(' - ')
-    num_fields = len(fields)
-    found = {}
-    for field in md_fields:
-        print('Working on', field, song_title)
-        val = []
-        while len(song_title) > 0:
-            val.append(song_title.pop(0))
-            l = ' - '.join(val)
-            if field == 'artist':
-                a = ctx.queries.get_artist(l)
-            elif field == 'title':
-                a = ctx.queries.get_title(found['artist'].id, l)
-            elif field == 'album':
-                if type(found['artist']) == list:
-                    for art in found['artist']:
-                        a = ctx.queries.get_album(art.id, l)
-                        if a.count() > 0:
-                            break
-                else:
-                    a = ctx.queries.get_album(found['artist'].id, l)
-            else:
-                print(f'ERROR: Unknow field: {field}')
-            if a.count() > 0:
-                if a.count() > 1:
-                    print('found more than one match for', field)
-                    found[field] = a.all()
-                else:
-                    found[field] = a.one()
-                break
-
-    titles = set()
-    if type(found['title']) == list:
-        for title in found['title']:
-            titles.add(title.title)
-    else:
-        titles.add(found['title'].title)
-    albums = set()
-    if type(found['album']) == list:
-        for album in found['album']:
-            albums.add(album.album.prename)
-    else:
-        albums.add(found['album'].album.prename)
-    if len(titles) == 1:
-        titles = titles.pop()
-    if len(albums) == 1:
-        albums = albums.pop()
-
-    song = ctx.queries.get_song_by_ata(found['artist'].fullname,
-                                       titles, albums)
-    if song.count() == 0:
-        print('Giving up!')
-        return None, None, None
-    else:
-        s = song.one()
-        print('Found song id', s.id)
-    return found['artist'].fullname, titles, albums
-        
 def update_irc_songs(ctx=None, as_dj=None, info=None, found_info=None, no_updates=None, update_played_only=False, fuzzy_match=False, played_dj_name=None):
     requested_by = None
     song_lengths = []
@@ -309,24 +237,11 @@ while True:
         for d in djs:
             if d.lower() in played_dj_name.lower():
                 active_source['dj_db'] = d.lower()
-                found_title_info = find_info(djs[d.lower()].context, active_source['title'])
+                found_title_info = helpers.find_info(djs[d.lower()].context, active_source['title'])
         #as_dj = active_source.dj_db
 
         current_dj_info = None
         for d in djs:
-            tt = ("B-52's - Private Idaho - Dance This Mess Around - The Best Of",
-                  "Orchestre G.M.I - Groupement mobil d'intervention - Africa - Analog Africa n° 19 : Senegal 70",
-                  "The Byrds - Chimes of Freedom - The Byrds, Dylan, Bob - The Byrds - Greatest Hits")
-            if d == "sartre1":
-                print('Doing sartre')
-                for test_title in tt:
-                    sid = find_info(djs[d].context, test_title)
-                    print(f'{test_title} ID {sid}')
-                #s = djs[d].context.queries.get_song_by_id(id=173291)
-                #a = djs[d].context.queries.get_artist('The Byrds')
-                #print(a.count())
-                #print(s.artist_fullname)
-
             update_played_only = False
             if active_source['dj_db'] != d:
                 if active_source['dj_db'] == 'autodj':
