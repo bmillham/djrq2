d28a9269a8544230be1934ca95f1bbe1f1f52838
Brian Millham <bmillham@gmail.com>
2023-04-20 00:11:13 +0000

    Fixed bug where average seconds was not rounded

--- tools/helpers/sec_to_hms.py
+++ tools/helpers/sec_to_hms.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 
 def sec_to_hms(seconds):
-    s = str(timedelta(seconds=seconds)).split(':', 1)
+    s = str(timedelta(seconds=round(seconds))).split(':', 1)
     if int(s[0]) > 0:
         s[0] = f'{h:02}' # Force hour to be 2 digits
     else:
